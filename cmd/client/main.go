169478a6c732da5ceb59344453a329af68a7dd69
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-16 16:21:48 -0500

    Adds a text command to the client
    This commit adds a new text command to the client application.
    The command allows users to interact with the server via simple
    text-based commands. This expands the client's functionality
    and provides a basic communication method.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"github.com/unmango/go/cli"
 	"github.com/unstoppablemango/ouranosis/pkg/cmd"
+	"github.com/unstoppablemango/ouranosis/pkg/cmd/client"
 )
 
 func main() {
 	cmd := cmd.NewClient()
+	cmd.AddCommand(client.NewTextCommand())
+
 	if err := cmd.Execute(); err != nil {
 		cli.Fail(err)
 	}
