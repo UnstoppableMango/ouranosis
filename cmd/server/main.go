692ac6a64f3e501d17ad3e81c53c80bdb5dede38
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-15 23:12:32 -0500

    Durdlin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"net"
+	"os"
+	"os/signal"
 
 	"github.com/charmbracelet/log"
 	osv1alpha1 "github.com/unstoppablemango/ouranosis/gen/dev/unmango/ouranosis/v1alpha1"
@@ -10,12 +13,30 @@ import (
 )
 
 func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:6969")
+	if err != nil {
+		log.Error("Failed to listen on address", "err", err)
+	}
+
 	srv := grpc.NewServer()
 	osv1alpha1.RegisterServerServiceServer(srv, server.Server{})
 
-	mux := http.NewServeMux()
-	mux.Handle("/", srv)
+	var cancel context.CancelFunc
+	ctx := context.Background()
+	ctx, cancel = signal.NotifyContext(ctx, os.Interrupt)
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			log.Info("Context completed, performing a graceful stop")
+			srv.GracefulStop()
+		}
+	}()
 
 	log.Info("ListenAndServing 0.0.0.0:6969")
-	_ = http.ListenAndServe("0.0.0.0:6969", mux)
+	if err := srv.Serve(lis); err != nil {
+		log.Error("Failed serving", "err", err)
+	}
+
+	cancel()
 }
