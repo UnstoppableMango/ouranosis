453a45a727e3adadf29abc5093df76c90ed402dc
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-16 15:13:00 -0500

    Moves client logic to cobra command
    Refactors the client logic into a cobra command for better CLI management.
    
    This change moves the client initialization and server interaction
    into a cobra command structure, improving the overall organization and
    extensibility of the client application.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,29 +1,13 @@
 package main
 
 import (
-	"context"
-
-	"github.com/charmbracelet/log"
-	ouranosisv1alpha1 "github.com/unstoppablemango/ouranosis/gen/dev/unmango/ouranosis/v1alpha1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"github.com/unmango/go/cli"
+	"github.com/unstoppablemango/ouranosis/pkg/cmd"
 )
 
 func main() {
-	conn, err := grpc.NewClient("0.0.0.0:6969",
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	if err != nil {
-		log.Error("Failed to create server connection", "err", err)
-	}
-
-	client := ouranosisv1alpha1.NewServerServiceClient(conn)
-
-	ctx := context.Background()
-	res, err := client.Reduce(ctx, &ouranosisv1alpha1.ReduceRequest{})
-	if err != nil {
-		log.Error("Failed to reduce from server", "err", err)
-	} else {
-		log.Info("Got response", "res", res)
+	cmd := cmd.NewClient()
+	if err := cmd.Execute(); err != nil {
+		cli.Fail(err)
 	}
 }
