4c604033248cb661423fa57a3c68d57d45486509
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-16 16:23:32 -0500

    Simplifies E2E tests
    Removes redundant lifecycle tests for server and client.
    
    Focuses on a single integration test to verify basic
    connectivity between client and server. This makes the
    e2e suite more focused and easier to maintain.

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,8 +1,6 @@
 package ouranosis_test
 
 import (
-	"os/exec"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
@@ -18,43 +16,5 @@ var _ = Describe("E2E Suite", func() {
 		Eventually(client.Err).Should(gbytes.Say(`Got response`))
 		Eventually(client).Should(gexec.Exit(0))
 		Eventually(server.Interrupt()).Should(gexec.Exit())
-	})
-
-	Context("Server", func() {
-		Describe("Lifecycle", Ordered, func() {
-			var serverSession *gexec.Session
-
-			It("should run", func() {
-				cmd := exec.Command(serverPath)
-				ses, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-
-				Eventually(ses.Err).Should(gbytes.Say(`ListenAndServing`))
-				serverSession = ses
-			})
-
-			It("should exit", func() {
-				Eventually(serverSession.Interrupt()).Should(gexec.Exit())
-			})
-		})
-	})
-
-	Context("Client", func() {
-		Describe("Lifecycle", Ordered, func() {
-			var clientSession *gexec.Session
-
-			It("should run", func() {
-				cmd := exec.Command(clientPath, "text")
-				ses, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-
-				Eventually(ses.Err).Should(gbytes.Say(`Got response`))
-				clientSession = ses
-			})
-
-			It("should exit", func() {
-				Eventually(clientSession).Should(gexec.Exit(0))
-			})
-		})
 	})
 })
