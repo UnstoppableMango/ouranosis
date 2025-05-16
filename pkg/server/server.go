6499584fbb502cfde670b9059d97385d110fb061
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-16 16:21:20 -0500

    Implements Reduce endpoint functionality
    Adds basic implementation for the Reduce endpoint.
    
    This change provides a starting point for the server's
    primary functionality. It receives a request, and returns
    a response containing the request's state and an empty list
    of server events.
    
    A constructor function `NewServer` is introduced to create a
    new instance of the `Server` struct.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -10,6 +10,13 @@ type Server struct {
 	osv1alpha1.UnimplementedServerServiceServer
 }
 
-func (Server) Reduce(context.Context, *osv1alpha1.ReduceRequest) (*osv1alpha1.ReduceResponse, error) {
-	return nil, nil
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s Server) Reduce(ctx context.Context, req *osv1alpha1.ReduceRequest) (*osv1alpha1.ReduceResponse, error) {
+	return &osv1alpha1.ReduceResponse{
+		State:  req.State,
+		Events: []*osv1alpha1.ServerEvent{},
+	}, nil
 }
