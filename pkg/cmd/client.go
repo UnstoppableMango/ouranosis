1098fdafbf5708515979be4b74f331f60f4c1fd2
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-16 16:20:38 -0500

    Introduces text-based client command
    Adds a new command for interacting with the application
    via a text-based interface.
    
    This involves creating a new cobra command and integrating
    it with the bubbletea TUI framework for user interaction.

--- pkg/cmd/client.go
+++ pkg/cmd/client.go
@@ -1,34 +1,23 @@
 package cmd
 
 import (
-	"context"
-
-	"github.com/charmbracelet/log"
+	tea "github.com/charmbracelet/bubbletea/v2"
 	"github.com/spf13/cobra"
-	ouranosisv1alpha1 "github.com/unstoppablemango/ouranosis/gen/dev/unmango/ouranosis/v1alpha1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"github.com/unmango/go/cli"
+	"github.com/unstoppablemango/ouranosis/pkg/model"
 )
 
 func NewClient() *cobra.Command {
 	return &cobra.Command{
 		Use: "client",
 		Run: func(cmd *cobra.Command, args []string) {
-			conn, err := grpc.NewClient("0.0.0.0:6969",
-				grpc.WithTransportCredentials(insecure.NewCredentials()),
+			m := model.NewClient()
+			prog := tea.NewProgram(m,
+				tea.WithContext(cmd.Context()),
+				tea.WithAltScreen(),
 			)
-			if err != nil {
-				log.Error("Failed to create server connection", "err", err)
-			}
-
-			client := ouranosisv1alpha1.NewServerServiceClient(conn)
-
-			ctx := context.Background()
-			res, err := client.Reduce(ctx, &ouranosisv1alpha1.ReduceRequest{})
-			if err != nil {
-				log.Error("Failed to reduce from server", "err", err)
-			} else {
-				log.Info("Got response", "res", res)
+			if _, err := prog.Run(); err != nil {
+				cli.Fail(err)
 			}
 		},
 	}
