62c711e61c1ff77b98b3bdb0da21f685cd6f8a61
Erik Rasmussen <mmhmm.ghetto@gmail.com>
2025-07-26 19:10:30 -0500

    Move around

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "math/rand/v2"
+import (
+	"fmt"
+	"math/rand/v2"
+	"time"
+)
 
 type Event struct{}
 
@@ -13,12 +17,31 @@ type Brain struct {
 
 func (b Brain) Apply(e Entity, _ []Event) (Entity, []Event) {
 	if rand.Float32() < 0.3 {
-		e.X++
+		if rand.Float32() > 0.5 {
+			e.X++
+		} else {
+			e.X--
+		}
+	}
+
+	if rand.Float32() < 0.3 {
+		if rand.Float32() > 0.5 {
+			e.Y++
+		} else {
+			e.Y--
+		}
 	}
 
 	return e, nil
 }
 
 func main() {
+	e := Entity{}
+	b := Brain{}
 
+	for {
+		time.Sleep(1 * time.Second)
+		e, _ = b.Apply(e, nil)
+		fmt.Printf("x: %d, y: %d\n", e.X, e.Y)
+	}
 }
